e26190cf31ee2990d5f94e1542d6a71fd2ddfc1f
Ganesh reddy <113967895+ganesh192110023@users.noreply.github.com>
2024-07-10 17:16:59 +0530

    decimal.java

--- decimaltobinary/src/decimal.java
+++ decimaltobinary/src/decimal.java
@@ -5,6 +5,8 @@ public static void main(String [] args)
 {
     Scanner s=new Scanner(System.in);
     int n=s.nextInt();
+
+    //converting integer to binary
     String binary = Integer.toBinaryString(n);
     System.out.println(binary);
 
